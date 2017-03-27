3b0522c50668f8b1e98817e2bb3b6fb7f1389929
Diego Diaz <berith.py@gmail.com>
2017-03-27 17:54:04 -0400

    Added MongoDB logging and request via api 2.0.
    Fixes

--- cotiapp.py
+++ cotiapp.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
-from flask import Flask, Response, render_template
+from flask import Flask, Response, render_template, request
 from flask_cors import CORS
 
-from coti import create_json, write_output
+from coti import create_json, write_output, receive_mongo
 
 app = Flask(__name__, static_url_path='')
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
@@ -13,8 +13,8 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/api/1.0/")
-def api_root():
+@app.route("/api/1.0/", methods=['POST', 'GET'])
+def api_root_1():
     response = ""
     try:
         with open('dolar.json', 'r') as f:
@@ -24,6 +24,20 @@ def api_root():
         write_output()
     return Response(response=response, status=200, mimetype='application/json')
 
+
+@app.route("/api/2.0/", methods=['POST', 'GET'])
+def api_mongo():
+    response = ""
+    dictionary = request.json
+    try:
+        if request.json:
+            response = receive_mongo(values=dictionary)
+        else:
+            response = receive_mongo(values={})
+    except IOError:
+        response = create_json()
+        write_output()
+    return Response(response=response, status=200, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(debug=True)
