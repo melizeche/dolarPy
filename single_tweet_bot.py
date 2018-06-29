4d9eb13dc5a85e652f5155ebf6c2e6544b24a431
Marcelo Elizeche Landó <marcelo.elizeche@adroll.com>
2018-06-29 17:36:05 -0400

    Added whitespace for better formatting

--- single_tweet_bot.py
+++ single_tweet_bot.py
@@ -11,25 +11,25 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 dolarjson = json.loads(get_output())
-updated = datetime.strptime(dolarjson['updated'], '%Y-%m-%d %H:%M:%S').strftime('📅%d/%m ⏳%H:%M')
+updated = datetime.strptime(dolarjson['updated'], '%Y-%m-%d %H:%M:%S').strftime('📅 %d/%m ⏳%H:%M')
 
 response =  updated + "\n\n" \
-            "💱\n■Cambios Chaco:\n"\
+            "💱\n■ Cambios Chaco:\n"\
             "Compra: " + "{:,}".format(dolarjson['dolarpy']['cambioschaco']['compra']).replace(',','.').replace('.0','') + \
             " | Venta: " + "{:,}".format(dolarjson['dolarpy']['cambioschaco']['venta']).replace(',','.').replace('.0','') + \
-            "\n■Alberdi:\n" \
+            "\n■ Alberdi:\n" \
             "Compra: " + "{:,}".format(dolarjson['dolarpy']['cambiosalberdi']['compra']).replace(',','.').replace('.0','') + \
             " | Venta: " + "{:,}".format(dolarjson['dolarpy']['cambiosalberdi']['venta']).replace(',','.').replace('.0','') + \
-            "\n■MyD Cambios:\n" \
+            "\n■ MyD Cambios:\n" \
             "Compra: " + "{:,}".format(dolarjson['dolarpy']['mydcambios']['compra']).replace(',','.').replace('.0','') + \
             " | Venta: " + "{:,}".format(dolarjson['dolarpy']['mydcambios']['venta']).replace(',','.').replace('.0','') + \
-            "\n■Maxicambios:\n" \
+            "\n■ Maxicambios:\n" \
             "Compra: " + "{:,}".format(dolarjson['dolarpy']['maxicambios']['compra']).replace(',','.').replace('.0','') + \
             " | Venta: " + "{:,}".format(dolarjson['dolarpy']['maxicambios']['venta']).replace(',','.').replace('.0','') + \
-            "\n\n🏛\n■BCP:\n"\
+            "\n\n🏛\n■ BCP:\n"\
             "Compra: " + "{:,}".format(int(dolarjson['dolarpy']['bcp']['compra'])).replace(',','.') + \
             " | Venta: " + "{:,}".format(int(dolarjson['dolarpy']['bcp']['venta'])).replace(',','.') + \
-            "\n■SET:\n" \
+            "\n■ SET:\n" \
             "Compra: " + "{:,}".format(int(dolarjson['dolarpy']['set']['compra'])).replace(',','.') +\
             " | Venta: " + "{:,}".format(int(dolarjson['dolarpy']['set']['venta'])).replace(',','.')
 
